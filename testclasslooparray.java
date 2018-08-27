bbedd4bcb6d2d517c6827cb530c7c437d4759b23
mdtaque <rifattaque@gmail.com>
2018-08-27 17:38:10 -0400

    nweweww

---
+++ testclasslooparray.java
@@ -0,0 +1,13 @@
+package loopNarray;
+
+public class testclasslooparray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		matrixx mat = new matrixx();
+		mat.fillmatrix();
+		mat.readmatrix();
+
+	}
+
+}
